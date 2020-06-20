3c1b47e101eb8555419ac965f9afcc715f29a13c
vic66 <pieryvic@gmail.com>
2020-06-20 14:33:57 +0200

    Configuration de notre React app.

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-west-1",
+      BUCKET: "notes-tuto"
+    },
+    apiGateway: {
+      REGION: "eu-west-1",
+      URL: "https://721nylw7sf.execute-api.eu-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-west-1",
+      USER_POOL_ID: "eu-west-1_s9iXRLjEa",
+      APP_CLIENT_ID: "51lvfhjg3u0cn7r44himjmjobr",
+      IDENTITY_POOL_ID: "eu-west-1:ee4d5541-6a29-4b44-a9bf-479df23214c7"
+    }
+  };
